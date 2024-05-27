18ce65b75ea0483c64bd80f89f82b428556297c1
Stewart Lynch <slynch@createchsol.com>
2024-05-26 20:59:29 -0700

    Initial Commit

---
+++ Custom Timer/Custom_TimerApp.swift
@@ -0,0 +1,22 @@
+//
+// Created for Custom Timer
+// by  Stewart Lynch on 2024-05-26
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+@main
+struct Custom_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
